3cb8d086fe07b0f7f746ef4a74f4f3047247c3cb
justineSimmet <justine.simmet@gmail.com>
2017-02-10 09:53:11 +0100

    maj 10 gitignore

---
+++ admin_datart/login.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('includes/config.php');
+require_once('includes/functions.php');
+require_once('classes/user.php');
+
+?>
+
+<div class="container-fluid">
+
+	<div class="row">
+		
+		<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12">
+
+			<div class="row">
+				<img src="assets/images/datart-rvb.png" alt=/>
+			</div>
+			
+		</div>
+
+
+	</div>
+	
+</div>
